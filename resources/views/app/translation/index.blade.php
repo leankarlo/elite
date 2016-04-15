19545d42f2656c0333b5fa4b22e2b0031dffdb6f
Lean Karlo Corpuz <lean@teknolohiya.ph>
2016-04-15 18:32:13 +0800

    New 	- LAYOUT 		-- Interpretation Page 		-- Translation Page

--- resources/views/app/translation/index.blade.php
+++ resources/views/app/translation/index.blade.php
@@ -26,6 +26,76 @@
     </div>
     <!-- PAGE HEADER END -->
 
+    <section id="about">
+        <div class="container service-bg">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <h1>About Us</h1>
+                    <p>Maecenas congue tortor luctus neque placerat, vestibulum suscipit turpis vulputate. Proin nec lorem molestie, sagittis dui et, aliquam leo. Vestibulum risus diam, malesuada efficitur malesuada.</p>
+
+                    <a href="#">read more...</a>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <img class="logo-default" src="../../../../../images/app/about/aboutimg1.png" alt="Logo">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="aboutgrey">
+        <div class="container service-bg">
+            <div class="row">
+                
+                <div class="col-md-6 col-sm-6">
+                    <img class="logo-default" src="../../../../../images/app/about/aboutimg2.png" alt="Logo">
+                </div>
+
+                <div class="col-md-6 col-sm-6">
+                    <h1>Technology driven<br>People-centric</h1>
+                    <p>Maecenas congue tortor luctus neque placerat, vestibulum suscipit turpis vulputate. Proin nec lorem molestie, sagittis dui et, aliquam leo. Vestibulum risus diam, malesuada efficitur malesuada.</p>
+
+                    <a href="#">read more...</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section id="about">
+        <div class="container service-bg">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <h1>Price</h1>
+                    <p>Maecenas congue tortor luctus neque placerat, vestibulum suscipit turpis vulputate. Proin nec lorem molestie, sagittis dui et, aliquam leo. Vestibulum risus diam, malesuada efficitur malesuada.</p>
+
+                    <a href="#">read more...</a>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <img class="logo-default" src="../../../../../images/app/about/aboutimg3.png" alt="Logo">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="aboutgrey">
+        <div class="container service-bg">
+            <div class="row">
+                
+                <div class="col-md-6 col-sm-6">
+                    <img class="logo-default" src="../../../../../images/app/about/aboutimg4.png" alt="Logo">
+                </div>
+
+                <div class="col-md-6 col-sm-6">
+                    <h1>Languages</h1>
+                    <p>Maecenas congue tortor luctus neque placerat, vestibulum suscipit turpis vulputate. Proin nec lorem molestie, sagittis dui et, aliquam leo. Vestibulum risus diam, malesuada efficitur malesuada.</p>
+
+                    <a href="#">read more...</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
     <a href="#intro" class="go2top"><i class="fa fa-arrow-up"></i></a>
 @endsection
 
